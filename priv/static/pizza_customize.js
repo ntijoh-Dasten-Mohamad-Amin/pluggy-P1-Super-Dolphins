9f2d844c26261c14869d13fe2982d1d96f677d61
Johan <johan.svensson@elev.ga.ntig.se>
2025-09-11 15:33:19 +0200

    jscript to grab children from modal and bootstrap implementation

---
+++ priv/static/pizza_customize.js
@@ -0,0 +1,32 @@
+
+
+function pizza_check(button, modal) {
+    switch(button.id) {
+        case "Margherita":
+                //ta fram modal inline-input genom children från button id och fore-check:a dom när man klickar på knappen
+            break;
+        case "Capricciosa":
+                //ta fram modal inline-input genom children från button id och fore-check:a dom när man klickar på knappen
+            break;
+        case "Marinara":
+                //ta fram modal inline-input genom children från button id och fore-check:a dom när man klickar på knappen
+            break;
+        case "QUattro formaggi":
+                //ta fram modal inline-input genom children från button id och fore-check:a dom när man klickar på knappen
+            break;
+        case "Prosciutto e funghi":
+                //ta fram modal inline-input genom children från button id och fore-check:a dom när man klickar på knappen
+            break;
+        case "Ortolana":
+                //ta fram modal inline-input genom children från button id och fore-check:a dom när man klickar på knappen
+            break;
+        case "Quattro stagioni":
+                //ta fram modal inline-input genom children från button id och fore-check:a dom när man klickar på knappen
+            break;
+        case "Diavola":
+                //ta fram modal inline-input genom children från button id och fore-check:a dom när man klickar på knappen
+            break;
+    }
+    modal.querySelector(".modal-header").querySelector(".modal-title").innerHTML = button.id
+    
+}
