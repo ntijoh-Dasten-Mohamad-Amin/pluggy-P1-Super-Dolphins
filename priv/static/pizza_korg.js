c9d27223fd41c95e1cfcec1f3b8ed197d47c39b8
Noah Erenmalm <noah.erenmalm@itggot-h9ftijor.learnet.se>
2025-09-12 16:03:35 +0200

    påväg med kundvagn2

--- priv/static/pizza_korg.js
+++ priv/static/pizza_korg.js
@@ -7,3 +7,4 @@ for (let index = 0; index < localStorage.length; index++) {
     const clone = kundvagn_holder.cloneNode(true)
     document.appendChild(clone)
 }
+//ha med ta bort knapp och lägg till p taggar och göm localstorage i en input hidden
