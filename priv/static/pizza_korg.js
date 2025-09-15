1fd7b93201b5bc0c9621af0effa7c27b1fd831cc
Johan <johan.svensson@elev.ga.ntig.se>
2025-09-15 09:14:53 +0200

    Fredags pub

--- priv/static/pizza_korg.js
+++ priv/static/pizza_korg.js
@@ -1,10 +1,29 @@
 function test() {
+    // RemoveIndex = localStorage[PizzaOrderNr]
     console.log(localStorage)
+    let result;
+    for (let index = 0; index < localStorage.length - 1; index++) {
+        text = localStorage.getItem[index]
+        result = text.split(",");
+    }
+
+
+    console.log(result)
+    console.log(text)
+
+
+    document.getElementById(kund_text).children[0].innerHTML = result[0]
+    document.getElementById(kund_text).children[1].innerHTML = result[4] + result[5] + result[6]
+
+
+
+
 }
 const kundvagn_holder = document.getElementsByClassName("kundvagn_holder")
 
-for (let index = 0; index < localStorage.length; index++) {
-    const clone = kundvagn_holder.cloneNode(true)
-    document.appendChild(clone)
+function removepizza() {
+    // localStorage.removeItem(RemoveIndex)
+
 }
+
 //ha med ta bort knapp och lägg till p taggar och göm localstorage i en input hidden
